934f6db1780129fbb2b9adeed1c1ae4052541a9c
ChaimZhu <zhuchenming@pjlab.org.cn>
2024-10-01 16:59:31 +0800

    fix scripts

--- scripts/convert_zero_to_fp32.sh
+++ scripts/convert_zero_to_fp32.sh
@@ -1,8 +1,2 @@
-srun -p mozi-S1 \
-    --gres=gpu:1 \
-    --quotatype=auto \
-    --ntasks=1 \
-    --ntasks-per-node=1 \
-    --cpus-per-task=12 \
-    --kill-on-bad-exit=1 \
-    python runs/reground3d-7b/ckpt_model/zero_to_fp32.py ./runs/reground3d-7b/ckpt_model ./runs/reground3d-7b/ckpt_model/pytorch_model.bin --tag global_step1000
+#!/bin/bash
+python runs/reground3d-7b/ckpt_model/zero_to_fp32.py ./runs/reground3d-7b/ckpt_model ./runs/reground3d-7b/ckpt_model/pytorch_model.bin --tag global_step1000
